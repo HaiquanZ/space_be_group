2ff34fb95c999e7bcd976f0d27c062b22d2b9a01
HaiquanZ <nn764562@gmail.com>
2024-04-23 22:39:55 +0700

    add-member

--- api/controllers/groupController.js
+++ api/controllers/groupController.js
@@ -1,22 +1,52 @@
-const GroupService = require("../services/groupService")
+const GroupService = require("../services/groupService");
 
 module.exports = (app) => {
-    const groupSrv = new GroupService();
+  const groupSrv = new GroupService();
 
-    app.post('/create', async(req, res, next) =>{
-        try{
-            const {name, img, describe} = req.body;
+  app.post("/create", async (req, res, next) => {
+    try {
+      const { name, img, describe } = req.body;
 
-            const user = {
-                avt: req.body.user.avatar,
-                name: req.body.user.name,
-                id: req.body.user.id
-            }
-            const result = await groupSrv.CreateGroup({name, img, describe, user});
-            return res.json(result);
-        }catch(err){
-            console.log(err);
-            next(err);
+      const user = {
+        avt: req.body.user.avatar,
+        name: req.body.user.name,
+        id: req.body.user.id,
+      };
+      const result = await groupSrv.CreateGroup({ name, img, describe, user });
+      return res.json({
+        status: 'success',
+        data: {
+            message: 'Group created successfully!',
+            group: result
         }
-    })
-}
+      });
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  });
+
+  app.post("/add-member", async (req, res, next) => {
+    try {
+      //console.log(req.body.userId[0]);
+      let users = [];
+      users = req.body.users;
+
+      const result = await groupSrv.AddMember({
+        groupId: req.body.groupId,
+        users,
+      });
+      if (result) {
+        return res.status(201).json({
+          status: "success",
+          data: {
+            message: "Add member successfully!",
+          },
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      next(err);
+    }
+  });
+};
--- api/services/groupService.js
+++ api/services/groupService.js
@@ -1,24 +1,44 @@
 const { GroupModel } = require("../models");
 
 class GroupService {
-    async CreateGroup(data){
-        try{
-            const group = new GroupModel({
-                name: data.name,
-                img: data.img,
-                describe: data.describe,
-                members: [
-                    { avt: data.user.avt, name: data.user.name, id: data.user.id, isAdmin: true}
-                ]
-            });
+  async CreateGroup(data) {
+    try {
+      const group = new GroupModel({
+        name: data.name,
+        img: data.img,
+        describe: data.describe,
+        members: [
+          {
+            avt: data.user.avt,
+            name: data.user.name,
+            id: data.user.id,
+            isAdmin: true,
+          },
+        ],
+      });
 
-            const result = group.save();
-            return result;
-        }catch(err){
-            console.log(err);
-            next(err);
-        }
+      const result = group.save();
+      return result;
+    } catch (err) {
+      console.log(err);
+      next(err);
     }
+  }
+
+  async AddMember(data) {
+    try {
+      const result = GroupModel.findOneAndUpdate(
+        { _id: data.groupId },
+        { $push: { members: { $each: data.users } } },
+        { new: true }
+      );
+
+      return result;
+    } catch (err) {
+      console.log(err);
+      //next(err);
+    }
+  }
 }
 
 module.exports = GroupService;
